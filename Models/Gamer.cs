de9bb0becf6de3d978e8e16b9b10bc706d2ad9a1
MrFox505 <d61smirnov@gmail.com>
2023-07-14 12:30:32 +0300

    Add new function

--- Models/Gamer.cs
+++ Models/Gamer.cs
@@ -3,9 +3,9 @@
     public class Gamer
     {
         public int Id { get; set; }
-        public string FullName { get; set; }
+        public string FullName { get; set; }//переделать на 3 поля и сделать собираемое скрытое
         public int Balance { get; set; }
-        public DateTime RegistrationDate { get; set; }
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
         public string Status { get; set; }
     }
 }
