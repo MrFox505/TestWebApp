33cd16d5c318f21b78779cb13575f0f5ce4b3a1c
MrFox505 <d61smirnov@gmail.com>
2023-07-17 22:00:10 +0300

    Update

--- Models/Bet.cs
+++ Models/Bet.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public int GamerId { get; set; }
         public int Sum { get; set; }
-        public DateTime DateBet { get; set; }
+        public DateTime DateBet { get; set; } = DateTime.Now;
         public float Win { get; set; }
-        public DateTime CalculationDate { get; set; }
+        public DateTime CalculationDate { get; set; } = DateTime.Now;
     }
 }
--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int GamerId { get; set; }
         public int Sum { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
         public string TypeOperation { get; set; }
     }
 }
